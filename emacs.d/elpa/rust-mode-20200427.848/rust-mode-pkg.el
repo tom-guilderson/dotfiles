27a5a5bc20bd1c981da78ae119ba294f45b82372
tom.guilderson <tom.guilderson@datadoghq.com>
2020-05-11 19:23:55 -0400

    add all my current elisp packages

---
+++ emacs.d/elpa/rust-mode-20200427.848/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20200427.848" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "1f8a7631da96423fc08734fcee8db6055a3d0e31" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
